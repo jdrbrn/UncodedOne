78df06ff432ff0c7eed26b9ee0aa4301299da255
jdrbrn <jdrbrn@users.noreply.github.com>
2021-09-12 14:48:22 -0400

    Add Battle Class
    Separates out the battle logic from Program.cs

---
+++ Classes/Battle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TheUncodedOne
+{
+    class Battle
+    {
+        private Party _party1;
+        private Party _party2;
+
+        public Battle(Party party1, Party party2)
+        {
+            _party1 = party1;
+            _party2 = party2;
+        }
+
+        public void Run()
+        {
+            //Basic taking turns
+            while (true)
+            {
+                _party1.TakeTurn();
+                _party2.TakeTurn();
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,19 +2,13 @@
 using System.Threading;
 using TheUncodedOne;
 
-Party Heroes = new Party();
-Party Monsters = new Party();
-Monsters.AddMember(new Skeleton());
-
+Party Heroes = new Party(new ComputerPlayer());
 Console.Write("What is your name? ");
 string input = Console.ReadLine();
 Heroes.AddMember(new TrueProgrammer(input));
 
-//Basic taking turns
-while (true)
-{
-    Heroes.TakeTurn();
-    Monsters.TakeTurn();
-    Thread.Sleep(500);
-}
+Party Monsters = new Party(new ComputerPlayer());
+Monsters.AddMember(new Skeleton());
 
+Battle game = new Battle(Heroes, Monsters);
+game.Run();
