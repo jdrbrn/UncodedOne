e71a7fcd7e4267de02e7bbaa6651af5d4dd6eca1
jdrbrn <jdrbrn@users.noreply.github.com>
2021-09-12 14:17:10 -0400

    Add basic Action support
    Adds Action interface
    Moves doing nothing from Character.Run to a seperate Action

---
+++ Classes/Actions/DoNothing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheUncodedOne
+{
+    class DoNothing : IAction
+    {
+        public void Run(Character character)
+        {
+            Console.WriteLine($"{character.Name} did NOTHING");
+            Console.WriteLine();
+        }
+    }
+}
--- Classes/Characters/Character.cs
+++ Classes/Characters/Character.cs
@@ -9,18 +9,19 @@ namespace TheUncodedOne
     class Character
     {
         public string Name { get; private set; }
+        private List<IAction> _actions = new List<IAction>();
 
         public Character(string name)
         {
             Name = name;
+            _actions.Add(new DoNothing());
         }
 
         public void TakeTurn()
         {
             Console.WriteLine($"It is {Name}'s turn...");
             // Do nothing
-            Console.WriteLine($"{Name} did NOTHING");
-            Console.WriteLine();
+            _actions[0].Run(this);
         }
     }
 }
