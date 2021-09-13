d3ca4b319d2c7a34a3ae84ee8f433132171d365c
jdrbrn <jdrbrn@users.noreply.github.com>
2021-09-12 22:12:20 -0400

    Implements Gametypes
    Game now has Human v Computer, Computer v Computer, and Human v Human modes.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 using TheUncodedOne;
 
+Console.WriteLine("How do you want to fight The Uncoded One?");
+Console.WriteLine("1 - Human vs Computer");
+Console.WriteLine("2 - Computer vs Computer");
+Console.WriteLine("3 - Human vs Human");
+int gameTypeChoice;
+string input;
+do
+{
+    Console.Write("What do you want to do? ");
+    input = Console.ReadLine();
+} while (!Int32.TryParse(input, out gameTypeChoice) && (gameTypeChoice >= 1 && gameTypeChoice <= 2));
+
 Party Heroes = new Party();
-Console.Write("What is your name? ");
-string input = Console.ReadLine();
-Heroes.Members.Add(new TrueProgrammer(input));
-HumanPlayer HeroPlayer = new HumanPlayer(Heroes);
+Console.Write("What is the name of the hero? ");
+string name = Console.ReadLine();
+Heroes.Members.Add(new TrueProgrammer(name));
+Player HeroPlayer;
+if (gameTypeChoice != 2)
+{
+    HeroPlayer = new HumanPlayer(Heroes);
+}
+else
+{
+    HeroPlayer = new ComputerPlayer(Heroes);
+}
 
 Party MonstersRound1 = new Party();
 MonstersRound1.Members.Add(new Skeleton());
-ComputerPlayer EnemyRound1 = new ComputerPlayer(MonstersRound1);
 
-Party Monsters2 = new Party();
-Monsters2.Members.Add(new Skeleton());
-Monsters2.Members.Add(new Skeleton());
-ComputerPlayer EnemyRound2 = new ComputerPlayer(Monsters2);
+Party MonstersRound2 = new Party();
+MonstersRound2.Members.Add(new Skeleton());
+MonstersRound2.Members.Add(new Skeleton());
 
 Party MonstersRound3 = new Party();
 MonstersRound3.Members.Add(new UncodedOne());
-ComputerPlayer EnemyRound3 = new ComputerPlayer(MonstersRound3);
 
-Player[] EnemyTeams = new Player[] { EnemyRound1, EnemyRound2, EnemyRound3 };
+Party[] EnemyTeams = new Party[] { MonstersRound1, MonstersRound2, MonstersRound3 };
 
 for (int i=0; i<EnemyTeams.Length; i++)
 {
-    new Battle(HeroPlayer, EnemyTeams[i]).Run();
+    Player Enemy;
+    if (gameTypeChoice==3)
+    {
+        Enemy = new HumanPlayer(EnemyTeams[i]);
+    }
+    else
+    {
+        Enemy = new ComputerPlayer(EnemyTeams[i]);
+    }
+    new Battle(HeroPlayer, Enemy).Run();
     Console.WriteLine();
     if (HeroPlayer.Party.Members.Count == 0) break;
     if (i<EnemyTeams.Length-1)
