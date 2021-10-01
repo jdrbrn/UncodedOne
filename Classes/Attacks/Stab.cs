8813aa43f623edd5575303b3710a503fd882abcd
jdrbrn <jdrbrn@users.noreply.github.com>
2021-10-01 09:39:01 -0400

    Adds Dagger
    Implements Dagger gear and Stab attack

---
+++ Classes/Attacks/Stab.cs
@@ -0,0 +1,14 @@
+﻿namespace TheUncodedOne.Attacks
+{
+    class Stab : IAttack
+    {
+        public string Name => "Stab";
+        public AttackData Data
+        {
+            get
+            {
+                return new AttackData { damage = 1 };
+            }
+        }
+    }
+}
---
+++ Classes/Gear/Dagger.cs
@@ -0,0 +1,10 @@
+﻿namespace TheUncodedOne.Gear
+{
+    class Dagger : IGear
+    {
+        private IAttack _attack = new TheUncodedOne.Attacks.Stab();
+        public string Name => "Dagger";
+        public IAttack Attack => _attack;
+
+    }
+}
