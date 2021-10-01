8b64c2372403742de4efe2b1da7ac69bcf9ebdd0
jdrbrn <jdrbrn@users.noreply.github.com>
2021-10-01 09:37:39 -0400

    Add Sword
    Adds Sword gear and Slash attack

---
+++ Classes/Attacks/Slash.cs
@@ -0,0 +1,14 @@
+﻿namespace TheUncodedOne.Attacks
+{
+    class Slash : IAttack
+    {
+        public string Name => "Slash";
+        public AttackData Data
+        {
+            get
+            {
+                return new AttackData { damage = 2 };
+            }
+        }
+    }
+}
---
+++ Classes/Gear/Sword.cs
@@ -0,0 +1,10 @@
+﻿namespace TheUncodedOne.Gear
+{
+    class Sword : IGear
+    {
+        private IAttack _attack = new TheUncodedOne.Attacks.Slash();
+        public string Name => "Sword";
+        public IAttack Attack => _attack;
+
+    }
+}
