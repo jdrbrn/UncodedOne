8802f52b1090b1b7ed261bea9b7ad2f472525f3d
jdrbrn <jdrbrn@users.noreply.github.com>
2021-10-01 09:49:16 -0400

    Add gear support to ComputerPlayer
    The Computer Player will now equip Gear and use Gear attacks

--- Classes/Players/ComputerPlayer.cs
+++ Classes/Players/ComputerPlayer.cs
@@ -15,6 +15,16 @@
             {
                 return new ItemAction(battle.GetParty(character).Items[0]);
             }
+            // If no gear equipped and gear is available, equip gear 50% of time
+            else if (character.EquippedGear == null && battle.GetParty(character).Gear.Count>0 && rng.Next(1)==0 )
+            {
+                return new GearAction(battle.GetParty(character).Gear[0]);
+            }
+            // If gear is equipped use the Gear based attacks
+            else if (character.EquippedGear != null)
+            {
+                return new AttackAction(character.EquippedGear.Attack, battle.GetEnemyParty(character).Members[0]);
+            }
             else
             {
                 return new AttackAction(character.StandardAttack, battle.GetEnemyParty(character).Members[0]);
